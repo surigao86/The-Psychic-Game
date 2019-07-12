7739f94f2d4ccc2f13a1c5177b1a98225f0b3559
Muey Surigao <muey.surigao@gmail.com>
2019-07-12 14:38:55 -0700

    Updated game

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+let letters = ["m", "u", "e", "y"];
+
+let guessedLetters = [];
+
+let letterToGuess = null;
+
+let guessesLeft = 9;
+
+let wins = 0;
+
+let losses =0;
+
+
+const updatedGuessLeft = () => {
+    document.querySelector("#guesses-left").innerHTML = guessesLeft;
+}
+
+const updatedLeterToGuest = () => {
+    letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+}
+
+const updatedGuessesSoFar = () => {
+    document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
+}
+    
+const reset = () => {
+    guessesLeft = 9;
+    guessedLetters = [];
+    updatedLeterToGuest (); 
+    updatedGuessLeft(); 
+    updatedGuessesSoFar();
+}
+
+updatedLeterToGuest();
+updatedGuessLeft();
+
+document.onkeydown = () => {
+    guessesLeft--;
+    let letter = event.key.toLowerCase();
+    //Then its going to add the guess into the array
+    guessedLetters.push(letter);
+
+    updatedGuessLeft();
+    updatedGuessesSoFar();
+
+    if(letter === letterToGuess){
+        wins++;
+        document.querySelector("#wins").innerHTML = wins;
+        reset();
+    }
+
+    if(guessesLeft ===0){
+        losses++;
+        document.querySelector("#losses").innerHTML = losses;
+        reset();
+    }
+}
